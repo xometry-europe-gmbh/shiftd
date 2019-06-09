87e32a445e9e7bbfb26ff2b79db8d3201377fb37
Alex Key <alexk3y@gmail.com>
2019-06-09 20:43:25 +0300

    Add bootstrap script for MinGW64

---
+++ scripts/bootstrap_mingw64.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+PLATFORM="$(uname -s)"
+if ! echo "${PLATFORM}" | grep MINGW64_NT; then
+    echo "ERROR: Unsupported platform (${PLATFORM})"
+    exit 1
+fi
+
+PACMAN="$(which pacman)"
+
+PACMAN_PACKAGES=(
+    "git"
+    "mc"
+    "base-devel"
+    "mingw-w64-x86_64-toolchain"
+    "mingw-w64-x86_64-cmake"
+)
+
+
+echo -e "\nBootstraping MinGW64...\n"
+
+echo -e "Updating packages (restart may be needed)\n==="
+"${PACMAN}" -Syuu
+echo
+
+echo -e "Setting up the development toolchain\n==="
+"${PACMAN}" -S --needed ${PACMAN_PACKAGES[*]}
+echo
+
+echo -e "Installing the latest Pip and Virtualenv\n==="
+if [[ ! -f "get-pip.py" ]]; then
+    curl -s "https://bootstrap.pypa.io/get-pip.py" -o get-pip.py
+fi
+python get-pip.py
+echo
+pip --version
+echo
+pip install -U virtualenv
+pip list
+
+echo -e "\nDONE"
